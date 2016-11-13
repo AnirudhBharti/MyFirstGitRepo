a2966a610f63361acf21cfdd22b741dfe6a3c72f
Ashish Saini <aashish11sep@live.com>
2016-11-14 01:48:10 +0530

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstAzureMVCApplication.Startup))]
+namespace MyFirstAzureMVCApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
